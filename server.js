f8ac5589fffc540945a6ad2786940d294da5bf7f
avendo <avendo@pop-os.localdomain>
2022-01-08 22:57:18 +0100

    Added static.json for heroku routing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const serveStatic = require("serve-static");
 const path = require('path');
 app = express();
 app.use(history({
-    verbose: true
+    verbose: false
 }))
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
