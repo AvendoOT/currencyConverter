1e375d3ffb5632bfd677cb705393bbec94624cf2
avendo <avendo@pop-os.localdomain>
2022-01-08 22:47:51 +0100

    Added history

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
+app.use(history({
+    verbose: true
+}))
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
